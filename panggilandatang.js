bf9c7b122bf6c573450536328ffdbc1cdc0b1acb
kivli <zulkivlilida@gmail.com>
2018-10-07 10:42:12 +0800

    Update panggilandatang.js

--- panggilandatang.js
+++ panggilandatang.js
@@ -26,7 +26,7 @@ if(x=="1"){xxx="/2017/12/profil-park-shin-hye.html"}
 if(x=="2"){xxx="/2017/12/sinopsis-drama-jepang-orphan-black.html"}
 if(x=="3"){xxx="/2017/12/sinopsis-drama-korea-just-between-lovers.html"}
 if(x=="4"){xxx="/2017/12/drama-korea-black-knight-man-who-guards.html"}
-if(x=="5"){xxx="/2017/12/drama-korea-yang-akan-tayang-januari.html"}a_to_ve[i].href="https://oppaoppaa1.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2017/12/drama-korea-yang-akan-tayang-januari.html"}a_to_ve[i].href="https://oppaoppaa1.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
